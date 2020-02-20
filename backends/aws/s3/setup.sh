d7682ab8d183925b70845a8c152a1653851fd05e
Bryan Heden <bryan.heden1@chsinc.com>
2020-02-20 14:24:04 -0600

    s3 bucket creation

--- backends/aws/s3/setup.sh
+++ backends/aws/s3/setup.sh
@@ -2,7 +2,14 @@
 
 . ../../lib.sh
 
-check_args $# 1 "Usage: ./$0 <s3-bucket-name>"
+check_args $# 1 "Usage: ./$0 <s3-bucket-name> <optional region>"
 check_deps aws
 
 name=$1
+region=""
+
+if [ x$2 != x ]; then
+    region="--region $2"
+fi
+
+aws s3api create-bucket --bucket $name $region
